ebf6cea29f4f4ef37595576996c2392b591267c8
Resep An <resepan100@gmail.com>
2023-11-08 10:23:33 -0800

    Jason.py

---
+++ importjason.py
@@ -0,0 +1,16 @@
+#
+import json
+
+
+#Ceate the data dictionary
+
+datat = {
+
+    'name': 'John Doe',
+    'age':30,
+    'city':'New york,NY',
+    'interests': ['dancing','cooking','video games'],
+    'is_student': True
+
+
+}
